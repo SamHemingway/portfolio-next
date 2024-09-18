4aac7b2ef048f2717baf7a93700a7025a0ced4b9
SamHemingway <hello@samhemingway.dev>
2024-09-18 13:50:14 -0400

    simplified heading.tsx

--- src/components/Primitives/Heading/Heading.tsx
+++ src/components/Primitives/Heading/Heading.tsx
@@ -1,38 +1,27 @@
-"use client";
-import React, { ForwardedRef } from "react";
+import React from "react";
 import styles from "./Heading.module.css";
-import { motion, MotionProps } from "framer-motion";
-import { AnimationContext } from "../../../contexts/AnimationProvider";
 
-interface HeaderProps extends MotionProps {
+interface HeaderProps {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
 }
 
-const Header = React.forwardRef<HTMLElement, HeaderProps>(
-  ({ level, children, ...delegated }, ref: ForwardedRef<HTMLElement>) => {
-    const TagPrimitive = `h${level || 1}` as keyof JSX.IntrinsicElements;
-    const { variants } = React.useContext(AnimationContext);
+function Header({ level, children, ...delegated }: HeaderProps) {
+  const TagPrimitive = `h${level || 1}`;
 
-    const MotionTag = motion(TagPrimitive as React.ElementType);
+  let computedStyles = `${styles.headerBase}`;
+  if (level === 1) computedStyles += ` ${styles.mainHeader}`;
 
-    let computedStyles = `${styles.headerBase}`;
-    if (level === 1) computedStyles += ` ${styles.mainHeader}`;
-
-    return (
-      <MotionTag
-        ref={ref}
-        className={computedStyles}
-        {...delegated}
-        variants={variants.fadeNoDelay}
-        initial="start"
-        animate="end"
-      >
-        {children}
-      </MotionTag>
-    );
-  }
-);
+  return (
+    // @ts-ignore
+    <TagPrimitive
+      className={computedStyles}
+      {...delegated}
+    >
+      {children}
+    </TagPrimitive>
+  );
+}
 
 Header.displayName = "Header";
 
