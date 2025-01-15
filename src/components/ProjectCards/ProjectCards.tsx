23dcb53c90601d0f4b1e5bc8ebb512a76483be66
SamHemingway <hello@samhemingway.dev>
2025-01-15 10:03:25 -0500

    fix: mobile styling for images in experience section

--- src/components/ProjectCards/ProjectCards.tsx
+++ src/components/ProjectCards/ProjectCards.tsx
@@ -44,8 +44,8 @@ function ProjectCards({
                     .url()}
                   alt="A screenshot of the project."
                   className={styles.cardImage}
-                  width={500}
-                  height={200}
+                  width={600}
+                  height={233}
                 />
               ) : null}
               <div className={styles.cardContent}>
